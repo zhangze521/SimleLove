5d500d97a35038cb1e4fd3f2145da68de7194c2d
lanou3g <lanou3g@lanou3gdeMac-mini.local>
2016-02-29 17:10:02 +0800

    初次大框架.

---
+++ SimpleLoveAPP/SimpleLoveAPP/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  SimpleLoveAPP
+//
+//  Created by lanou3g on 16/2/29.
+//  Copyright © 2016年 张泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
