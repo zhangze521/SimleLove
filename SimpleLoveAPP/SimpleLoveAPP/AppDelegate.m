bf0cbc36a37afa9ec4139a574280ec57836efc6c
lanou3g <953733823@qq.com>
2016-03-01 09:26:03 +0800

    3.1 融云

--- SimpleLoveAPP/SimpleLoveAPP/AppDelegate.m
+++ SimpleLoveAPP/SimpleLoveAPP/AppDelegate.m
@@ -16,6 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSLog(@"刘润东");
+    
     // Override point for customization after application launch.
     return YES;
 }
